9c43ef6070d39afc64e6fb8451f2906a7a59cf39
shubham-chawla <chawlashubham007@gmail.com>
2019-12-06 02:46:22 +0530

    feat: Adds mouseEvents

--- src/components/View360/index.js
+++ src/components/View360/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import * as THREE from 'three';
 
+const imgUrl = 'https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.jpg';
+// 'https://raw.githubusercontent.com/spite/THREE.CubemapToEquirectangular/master/about/pano-The%20Polygon%20Shredder-1471041904038.jpg'
+
 class View360 extends React.Component {
     componentDidMount() {
         let isUserInteracting = false,
@@ -21,7 +24,7 @@ class View360 extends React.Component {
         const geometry = new THREE.SphereBufferGeometry(500, 60, 40);
         // invert the geometry on the x-axis so that all of the faces point inward
         geometry.scale(-1, 1, 1);
-        const texture = new THREE.TextureLoader().load('https://live.staticflickr.com/3289/2294472375_d8b56e1023_h.jpg');
+        const texture = new THREE.TextureLoader().load(imgUrl);
         const material = new THREE.MeshBasicMaterial({ map: texture });
 
         const mesh = new THREE.Mesh(geometry, material);
@@ -32,6 +35,85 @@ class View360 extends React.Component {
         // document.body.appendChild( renderer.domElement );
         // use ref as a mount point of the Three.js scene instead of the document.body
         this.mount.appendChild(renderer.domElement);
+
+        const onWindowResize = () => {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        };
+
+        const onPointerStart = event => {
+            isUserInteracting = true;
+
+            const clientX = event.clientX || event.touches[0].clientX;
+            const clientY = event.clientY || event.touches[0].clientY;
+
+            onMouseDownMouseX = clientX;
+            onMouseDownMouseY = clientY;
+
+            onMouseDownLon = lon;
+            onMouseDownLat = lat;
+        };
+
+        const onPointerMove = event => {
+            if (isUserInteracting === true) {
+                const clientX = event.clientX || event.touches[0].clientX;
+                const clientY = event.clientY || event.touches[0].clientY;
+
+                lon = (onMouseDownMouseX - clientX) * 0.1 + onMouseDownLon;
+                lat = (clientY - onMouseDownMouseY) * 0.1 + onMouseDownLat;
+            }
+        };
+
+        const onPointerUp = () => {
+            isUserInteracting = false;
+        };
+
+        const onDocumentMouseWheel = event => {
+            const fov = camera.fov + event.deltaY * 0.05;
+            camera.fov = THREE.Math.clamp(fov, 10, 75);
+            camera.updateProjectionMatrix();
+        };
+
+        // add event listeners here
+        document.addEventListener('mousedown', onPointerStart);
+        document.addEventListener('mousemove', onPointerMove);
+        document.addEventListener('mouseup', onPointerUp);
+
+        document.addEventListener('wheel', onDocumentMouseWheel);
+
+        document.addEventListener('touchstart', onPointerStart);
+        document.addEventListener('touchmove', onPointerMove);
+        document.addEventListener('touchend', onPointerUp);
+
+        document.addEventListener('dragover', event => {
+            event.preventDefault();
+            event.dataTransfer.dropEffect = 'copy';
+        });
+
+        document.addEventListener('dragenter', () => {
+            document.body.style.opacity = 0.5;
+        });
+
+        document.addEventListener('dragleave', () => {
+            document.body.style.opacity = 1;
+        });
+
+        document.addEventListener('drop', event => {
+            event.preventDefault();
+
+            const reader = new FileReader();
+            reader.addEventListener('load', event => {
+                material.map.image.src = event.target.result;
+                material.map.needsUpdate = true;
+            });
+            reader.readAsDataURL(event.dataTransfer.files[0]);
+
+            document.body.style.opacity = 1;
+        });
+
+        window.addEventListener('resize', onWindowResize);
 
         const update = () => {
             if (isUserInteracting === false) {
@@ -55,6 +137,7 @@ class View360 extends React.Component {
 
             renderer.render(scene, camera);
         };
+
         const animate = () => {
             requestAnimationFrame(animate);
             update();
